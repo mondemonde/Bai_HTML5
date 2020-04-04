05783708840a1a3788669e65b5cf500556a126d0
Aaron Mangrobang <amangrobang@blastasia.com>
2020-04-04 21:24:48 +0800

    Finale

--- TRAINEEs/aaronMangrobang/Scripts/app.js
+++ TRAINEEs/aaronMangrobang/Scripts/app.js
@@ -2,3 +2,11 @@ function updateTime()
 {
     document.getElementById('hTime').innerHTML= new Date().toLocaleString();
 }
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
